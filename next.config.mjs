cfd4432b0c876fc0a1cb8430c5f5099d8ec1aba2
ccrawford4 <calumcrawford9@gmail.com>
2024-10-19 13:10:29 -0700

    adjust middleware, etc for .swa files

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,20 @@
 import MillionLint from "@million/lint";
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/((?!api|login|logout|register|_next/static|_next/image|favicon.ico).*)', // Exclude paths that don't need protection
+          destination: '/login',
+        },
+      ],
+    };
+  },
+};
 
 export default MillionLint.next({
   enabled: true,
   production: false,
-  rsc: true
+  rsc: true,
 })(nextConfig);
