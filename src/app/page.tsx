5bd21b16349087bd34e2b6cf63cead91cc9e2d9b
ccrawford4 <calumcrawford9@gmail.com>
2025-05-04 13:50:19 -0700

    feat(test): test words

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,10 +67,10 @@ export default function Home() {
 
   const getFirstName = (name: string | null | undefined) => {
     if (!name) {
-        return "";
+      return "";
     }
     if (name.split(" ").length > 1) {
-        return name.split(" ")[0];
+      return name.split(" ")[0];
     }
     return "";
   }
@@ -86,7 +86,7 @@ export default function Home() {
           )}
           <div className="relative text-center p-8 bg-white shadow-lg rounded-xl max-w-lg w-full">
             <h1 className="text-3xl font-semibold mb-4 text-gray-800">
-              {`Welcome ${getFirstName(session.user?.name)} 🎉`}
+              {`Welcome Test ${getFirstName(session.user?.name)} 🎉`}
             </h1>
             <h2 className="text-lg mb-6 text-gray-500">Search for a term</h2>
             <MemoizedSearch handleSearch={handleSearch} isLoading={isLoading} />
