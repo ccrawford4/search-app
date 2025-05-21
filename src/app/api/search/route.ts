0f44c6a8421d5fbdda28dd32646eca55c4028889
ccrawford4 <calumcrawford9@gmail.com>
2025-05-20 22:15:49 -0700

    feat(updates): added logs

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,36 +1,38 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-    console.log("Inside search");
-    const request = await req.json();
-    console.log("Request: ", request);
+  console.log("Inside search");
+  const request = await req.json();
+  console.log("Request: ", request);
 
 
-    const API_ENDPOINT = process.env.API_ENDPOINT;
-    console.log("API_ENDPOINT: ", API_ENDPOINT);
-    if (!API_ENDPOINT) {
-      return new NextResponse("Error: API_ENDPOINT not set", { status: 500 });
-    }
+  const API_ENDPOINT = process.env.API_ENDPOINT;
+  console.log("API_ENDPOINT: ", API_ENDPOINT);
+  if (!API_ENDPOINT) {
+    return new NextResponse("Error: API_ENDPOINT not set", { status: 500 });
+  }
 
-    try {
-      const response = await fetch(API_ENDPOINT,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            SearchTerm: request.searchQuery,
-          }),
-        }
-      );
-      if (!response.ok) {
-        console.log("Error: ", response.statusText);
+  try {
+    const response = await fetch(API_ENDPOINT,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          SearchTerm: request.searchQuery,
+        }),
       }
-      
-      const body = await response.json();
-      return new NextResponse(JSON.stringify(body), { status: 200 });
-    } catch (error) {
-      return new NextResponse("Error fetching results", { status: 500, statusText: error as string });
+    );
+    console.log("Response: ", response);
+    if (!response.ok) {
+      console.log("Error: ", response.statusText);
     }
+
+    const body = await response.json();
+    return new NextResponse(JSON.stringify(body), { status: 200 });
+  } catch (error) {
+    console.error("[ERROR]: ", error);
+    return new NextResponse("Error fetching results", { status: 500, statusText: error as string });
+  }
 }
